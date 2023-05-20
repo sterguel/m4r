8710e95801ff40b7733f48d93a6ab19034e08609
Sterge <sv819@ic.ac.uk>
2023-05-20 01:13:45 +0100

    patched major bugs in montgomery code

---
+++ slab/matmul_test.go
@@ -0,0 +1,39 @@
+package slab
+
+import (
+	"math/big"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func fillMatrix(A *IntMatrix, wl int) {
+	rand.Seed(time.Now().UnixNano())
+	for _, row := range A.Vals {
+		for _, val := range row {
+			b := make([]big.Word, wl)
+			for k := range b {
+				b[k] = big.Word(rand.Uint64())
+			}
+			val.SetBits(b)
+		}
+	}
+}
+func TestMul(t *testing.T) {
+	A := Make_Int_matrix(400, 300)
+	B := Make_Int_matrix(300, 450)
+	s := 4
+	fillMatrix(A, s)
+	fillMatrix(B, s)
+	Cv, _ := Mul_standard(A, B)
+	Ct, _ := Mul_modular_montgomery(A, B)
+	for i, row := range Cv.Vals {
+		for j, ent := range row {
+			if ent.Cmp(Ct.Vals[i][j]) != 0 {
+				t.Error("Incorrect value!!")
+				return
+			}
+		}
+	}
+
+}
---
+++ slab/util.go
@@ -0,0 +1,17 @@
+package slab
+
+func imax(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func imin(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func ceildiv(a int, b int) int {
+	return 1 + ((a - 1) / b)
+}
